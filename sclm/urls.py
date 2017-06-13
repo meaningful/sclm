490f3dc8ceffc9c252b0c604cf6735468a725cdd
liujie <liu_jie_1@126.com>
2017-06-13 20:47:46 +0800

    filer canonical urls

--- sclm/urls.py
+++ sclm/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello$', view.hello),
+    url(r'^filer/', include('filer.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
