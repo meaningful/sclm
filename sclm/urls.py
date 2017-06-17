9596ee3e90d2a23610e9548cd05328c2e34d9b5f
liu jie <liu_jie_1@126.com>
2017-06-17 19:26:06 +0800

    modify urls

--- sclm/urls.py
+++ sclm/urls.py
@@ -18,9 +18,13 @@ from django.contrib import admin
 from . import view
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello$', view.hello),
     url(r'^filer/', include('filer.urls')),
+   # url(r'^CollectStaticss/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATIC_ROOT}),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
